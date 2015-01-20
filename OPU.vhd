d6a3884c02ad0f78562f823291c1bec57a897123
Istvan Szonyi <pisti.szonyi@gmail.com>
2015-01-20 20:09:03 +0100

    Update OPU.vhd
    -added sticky bit expansion
    -added R and S injection value table
    
    -missing Ca3 and Cb3 generation (concatenating the appropriate fields)

--- OPU.vhd
+++ OPU.vhd
@@ -36,7 +36,7 @@ entity OPU is
     ca2 : in std_logic_vector(63 downto 0);
     cb2 : in std_logic_vector(71 downto 0);
     operation : in std_logic;
-    rounding : in std_logic;
+    rounding : in std_logic_vector(2 downto 0);
     sticky_bit : in std_logic;
     ca3 : out std_logic_vector(75 downto 0);   --19 digits
     cb3 : out std_logic_vector(75 downto 0)
@@ -45,7 +45,77 @@ end OPU;
 
 architecture Behavioral of OPU is
 
+signal ca3_s, cb3_s : std_logic_vector(75 downto 0);
+signal zero : std_logic_vector(3 downto 0);
+signal R, S : std_logic_vector(3 downto 0);
+signal sticky_digit : std_logic_vector(3 downto 0);
+
+
+
+
 begin
 
+
+--sticky digit generation
+-- will be changed with foor loop iteration later
+process(cb2, sticky_bit, operation)
+begin
+    if operation = '1' then
+        sticky_digit <= "000" & sticky_bit;
+    else
+         sticky_digit(0) <= cb2(0) or cb2(1) or cb2(2) or cb2(3) or sticky_bit;
+         sticky_digit(1) <= cb2(4) or cb2(5) or cb2(6) or cb2(7) or sticky_bit;
+         sticky_digit(2) <= cb2(8) or cb2(9) or cb2(10) or cb2(11) or sticky_bit;
+         sticky_digit(3) <= cb2(12) or cb2(13) or cb2(14) or cb2(15) or sticky_bit; 
+    end if;
+end process;                   
+             
+
+--injection values for different rounding modes
+--rounding mode: roundTowardsZero = 0000 
+-- ......
+--rounding mode: roundAwayZero = 1000
+process(operation, rounding)
+begin
+    case (rounding) is
+        when "000" => 
+            R <= "0000";
+            S <= "0000";
+        when "001" => 
+            R <= "0101";
+            S <= "0000";     
+        when "010" => 
+            R <= "0100";
+            S <= "1000";
+        when "011" => 
+            R <= "0101";
+            S <= "0000";  
+--Round Toward Poitive               
+-- need to review the sign bit/operation condition                   
+        when "100" => 
+            if operation = '1' then
+                R <= "0000";
+                S <= "0000";
+            else        
+                R <= "1000";
+                S <= "1000";
+            end if;
+ --Round toward Negative             
+        when "101" => 
+             if operation = '1' then
+                 R <= "1000";
+                 S <= "1000";
+                else        
+                  R <= "0000";
+                  S <= "0000";
+                end if;  
+          when "110" => 
+                  R <= "1000";
+                  S <= "1000";
+          when others => 
+                  R <= "0000";
+                  S <= "0000";
+        end case;              
+end process;
 
 end Behavioral;
