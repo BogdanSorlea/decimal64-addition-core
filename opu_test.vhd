aadd345bb45c28eb05509d64d765a61ae19dc1ae
Istvan Szonyi <pisti.szonyi@gmail.com>
2015-01-25 22:08:44 +0100

    updated according the new portlist

--- opu_test.vhd
+++ opu_test.vhd
@@ -38,13 +38,19 @@ architecture Behavioral of tb_opu is
 
  COMPONENT opu
     PORT(
-        ca2 : in std_logic_vector(63 downto 0);
-        cb2 : in std_logic_vector(71 downto 0);
-        operation : in std_logic;
-        rounding : in std_logic_vector(2 downto 0);
-        sticky_bit : in std_logic;
-        ca3 : out std_logic_vector(75 downto 0);   --19 digits
-        cb3 : out std_logic_vector(75 downto 0)
+           ca2 : in std_logic_vector(63 downto 0);
+           cb2 : in std_logic_vector(71 downto 0);
+           --for Sign_Inj
+           operation : in std_logic;
+           eop : in std_logic;
+           swap : in std_logic;
+           sa1, sb1 : in std_logic;
+           rounding : in std_logic_vector(2 downto 0);
+           --
+           sticky_bit : in std_logic;
+           ca3 : out std_logic_vector(75 downto 0);   --19 digits
+           cb3 : out std_logic_vector(75 downto 0);
+           sign_inj : out std_logic
         );
     END COMPONENT;
     
@@ -52,11 +58,12 @@ architecture Behavioral of tb_opu is
    --Inputs
    signal ca2 : std_logic_vector(63 downto 0) := (others => '0');
    signal cb2 : std_logic_vector(71 downto 0) := (others => '0');
-   signal operation, sticky_bit : std_logic := '0';
+   signal operation, sticky_bit, eop, sa1, sb1, swap : std_logic := '0';
    signal rounding : std_logic_vector(2 downto 0);
 
  	--Outputs
    signal ca3, cb3 : std_logic_vector(75 downto 0);
+   signal sign_inj : std_logic;
 
  
 BEGIN
@@ -65,11 +72,16 @@ BEGIN
    uut: opu PORT MAP (
           ca2 => ca2,
           cb2 => cb2,
+          eop => eop,
+          swap => swap, 
+          sa1 => sa1, 
+          sb1 => sb1,
           rounding => rounding,
           operation => operation,
           sticky_bit => sticky_bit, 
           ca3 => ca3,
-          cb3 => cb3
+          cb3 => cb3,
+          sign_inj => sign_inj
         );
  
 
@@ -79,14 +91,34 @@ BEGIN
       
       ca2 <= X"0000_0000_0000_0000";
       cb2 <= X"00_0000_0000_0000_0000";
+      eop <= '0';
+      swap <= '0';
+      sa1 <= '0';
+      sb1 <= '0';
       operation <= '0';
       rounding <= "000";
       sticky_bit <= '0';
      
       wait for 60ns;
+
+      ca2 <= X"0000_0000_0000_0000";
+      cb2 <= X"00_0000_0000_0000_0000";
+      eop <= '0';
+      swap <= '0';
+      sa1 <= '0';
+      sb1 <= '0';
+      operation <= '0';
+      rounding <= "000";
+      sticky_bit <= '0';
+      
+      wait for 60ns;
       
      ca2 <= X"0000_0000_0000_0001";
      cb2 <= X"00_0000_0000_0000_0001";
+     eop <= '0';
+     swap <= '0';
+     sa1 <= '0';
+     sb1 <= '0';
      operation <= '0';
      rounding <= "001";
      sticky_bit <= '0';
@@ -94,28 +126,28 @@ BEGIN
       wait for 60ns;
       
       ca2 <= X"0000_0000_0000_0010";
-      cb2 <= X"00_0000_0000_0000_0010";
+      cb2 <= X"00_0000_0000_0000_0011";
+      eop <= '0';
+      swap <= '0';
+      sa1 <= '1';
+      sb1 <= '0';     
       operation <= '0';
       rounding <= "010";
       sticky_bit <= '1';      
-      
-      wait for 60ns;
-      
-      ca2 <= X"0000_0000_0000_0011";
-      cb2 <= X"00_0000_0000_0000_0011";
-      operation <= '1';
-      rounding <= "101";
-      sticky_bit <= '1';      
 
-      wait for  60ns;
-      
-      ca2 <= X"0000_0000_0000_0000";
-      cb2 <= X"00_0000_0000_0000_0000";
-      operation <= '0';
-      rounding <= "000";
-      
-      sticky_bit <= '0';
-     wait;       
+        wait for 60ns;
+     ca2 <= X"0000_0000_0000_7777";
+     cb2 <= X"00_0000_0000_0000_1111";
+     eop <= '1';
+     swap <= '1';
+     sa1 <= '0';
+     sb1 <= '1';     
+     operation <= '0';
+     rounding <= "010";
+     sticky_bit <= '0';
+     
+     wait;
+   
    end process;
 
 
