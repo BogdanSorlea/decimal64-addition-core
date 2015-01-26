14113b545a5d65423bcceede0d4bf108e9ef8e67
Istvan Szonyi <pisti.szonyi@gmail.com>
2015-01-26 16:54:20 +0100

    final left_shifter

--- left_dbs.vhd
+++ left_dbs.vhd
@@ -32,16 +32,16 @@ use IEEE.NUMERIC_STD.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity LDBS is
+entity Left_DBS is
     Port (
      cas : in std_logic_vector(63 downto 0);
      lsa : in  std_logic_vector(4 downto 0);
      ca2 : out std_logic_vector(63 downto 0)   
      );
      
-end LDBS;
+end Left_DBS;
 
-architecture Behavioral of LDBS is
+architecture Behavioral of Left_DBS is
 
 signal cas_mux_4, cas_mux_3, cas_mux_2, cas_mux_1, cas_mux_0 : std_logic_vector(63 downto 0); 
 signal cas_sl_16, cas_sl_8, cas_sl_4, cas_sl_2, cas_sl_1 : std_logic_vector(63 downto 0);
@@ -49,19 +49,12 @@ signal cas_sl_16, cas_sl_8, cas_sl_4, cas_sl_2, cas_sl_1 : std_logic_vector(63 d
 
 begin
 
---left shifters for CA
-cas_sl_16 <= cas(47 downto 0) & "0000000000000000" ;
-cas_sl_8 <=  cas(55 downto 0) & "00000000" ;
-cas_sl_4 <=  cas(59 downto 0) & "0000";
-cas_sl_2 <=  cas(61 downto 0) & "00";
-
-
---cascaded muxes for CB
-cas_mux_4 <= cas              when lsa(4) = '0' else cas_sl_16;
-cas_mux_3 <= cas_mux_4        when lsa(3) = '0' else cas_sl_8;
-cas_mux_2 <= cas_mux_3        when lsa(2) = '0' else cas_sl_4; 
-cas_mux_1 <= cas_mux_2        when lsa(1) = '0' else cas_sl_2;
-cas_mux_0 <= cas_mux_1        when lsa(0) = '0' else cas;
+--cascaded muxes for CB and shift_values
+cas_mux_4 <= cas              when lsa(4) = '0' else X"0000_0000_0000_0000" ;
+cas_mux_3 <= cas_mux_4        when lsa(3) = '0' else cas(31 downto 0) & X"0000_0000" ;
+cas_mux_2 <= cas_mux_3        when lsa(2) = '0' else cas(47 downto 0) & X"0000"; 
+cas_mux_1 <= cas_mux_2        when lsa(1) = '0' else cas(55 downto 0) & X"00";
+cas_mux_0 <= cas_mux_1        when lsa(0) = '0' else cas(59 downto 0) & X"0";
 
 --cb2 output
 ca2 <= cas_mux_0;
