472343ce388a1abcb942a5d44642ea19a1e17cb0
Istvan Szonyi <pisti.szonyi@gmail.com>
2015-01-26 22:42:36 +0100

    i hate you

--- right_dbs.vhd
+++ right_dbs.vhd
@@ -74,16 +74,23 @@ begin
 --sticky bit generation
 --T bits
 
-
-process(cbs)
-  variable i : natural;
-  variable temp: std_logic := '0';
-begin
-  for i in cbs'range loop
-    temp :=  temp or cbs(i);
-  end loop;
-  t4 <= temp;
-end process;
+t4 <= cbs(0) or cbs(1) or cbs(2) or cbs(3) or cbs(4) or cbs(5) or cbs(6) or cbs(7)
+      or cbs(8) or cbs(9) or cbs(10) or cbs(11) or cbs(12) or cbs(13) or cbs(14) or cbs(15)
+      or cbs(16) or cbs(17) or cbs(18) or cbs(19) or cbs(20) or cbs(21) or cbs(22) or cbs(23)
+      or cbs(24) or cbs(25) or cbs_mux_3(26) or cbs(27) or cbs(28) or cbs(29) or cbs(30) or cbs(31)
+      or cbs(32) or cbs(33) or cbs(34) or cbs(35) or cbs(36) or cbs(37) or cbs(38) or cbs(39)
+      or cbs(40) or cbs(41) or cbs(42) or cbs(43) or cbs(44) or cbs(45) or cbs(46) or cbs(47)
+      or cbs(48) or cbs(49) or cbs(50) or cbs(51) or cbs(52) or cbs(53) or cbs(54) or cbs(55)
+      or cbs(56) or cbs(57) or cbs_mux_3(58) or cbs(59) or cbs(60) or cbs(61) or cbs(62) or cbs(63);
+--process(cbs)
+--  variable i : natural;
+--  variable temp: std_logic := '0';
+--begin
+--  for i in cbs'range loop
+--    temp :=  temp or cbs(i);
+--  end loop;
+--  t4 <= temp;
+--end process;
 
 t3 <= cbs_mux_4(0) or cbs_mux_4(1) or cbs_mux_4(2) or cbs_mux_4(3) or cbs_mux_4(4) or cbs_mux_4(5) or cbs_mux_4(6) or cbs_mux_4(7)
       or cbs_mux_4(8) or cbs_mux_4(9) or cbs_mux_4(10) or cbs_mux_4(11) or cbs_mux_4(12) or cbs_mux_4(13) or cbs_mux_4(14) or cbs_mux_4(15)
